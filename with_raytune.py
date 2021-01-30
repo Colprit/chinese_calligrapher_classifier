2936e33d787edec1d6e2268a3d0225b51249c089
mcolpus <matthew@colpus.co.uk>
2021-01-30 20:13:13 +0000

    added raytune for hyper parameter optimisation (#1)

---
+++ with_raytune.py
@@ -0,0 +1,219 @@
+import torch
+import torchvision as tv
+import matplotlib.pyplot as plt
+import numpy as np
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import os
+from functools import partial
+from torch.utils.data import random_split
+from ray import tune
+from ray.tune import CLIReporter
+from ray.tune.schedulers import ASHAScheduler
+
+
+def load_data(data_dir="./data"):
+    transform = tv.transforms.Compose([
+        tv.transforms.Grayscale(num_output_channels=1),
+        tv.transforms.ToTensor(),
+        tv.transforms.Normalize((0.5,), (0.5,))
+    ])
+
+    trainset = tv.datasets.ImageFolder(os.path.join(data_dir, 'train'),
+                                       transform)
+
+    testset = tv.datasets.ImageFolder(os.path.join(data_dir, 'test'),
+                                      transform)
+    
+    return trainset, testset
+
+
+class Net(nn.Module):
+    def __init__(self, lfinal, l1=120, l2=84):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(1, 6, 5)
+        self.pool1 = nn.MaxPool2d(3, 3)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.pool2 = nn.MaxPool2d(4, 4)
+        self.fc1 = nn.Linear(16 * 4 * 4, l1)
+        self.fc2 = nn.Linear(l1, l2)
+        self.fc3 = nn.Linear(l2, lfinal)
+    
+    def forward(self, x):
+        x = self.pool1(F.relu(self.conv1(x)))
+        x = self.pool2(F.relu(self.conv2(x)))
+        x = x.view(-1, 16 * 4 * 4)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+
+
+
+def train_net(config, checkpoint_dir=None, data_dir=None, num_epochs=1):
+    trainset, _ = load_data(data_dir)
+    test_abs = int(len(trainset) * 0.8)
+    train_subset, val_subset = random_split(
+        trainset, [test_abs, len(trainset) - test_abs])
+
+    trainloader = torch.utils.data.DataLoader(
+        train_subset,
+        batch_size=int(config["batch_size"]),
+        shuffle=True,
+        num_workers=4)
+    valloader = torch.utils.data.DataLoader(
+        val_subset,
+        batch_size=int(config["batch_size"]),
+        shuffle=True,
+        num_workers=4)
+
+    class_names = trainset.classes
+
+    net = Net(len(class_names), config["l1"], config["l2"])
+
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    print(device)
+    net.to(device)
+
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.SGD(net.parameters(), lr=config["lr"], momentum=0.9)
+
+    if checkpoint_dir:
+        model_state, opt_state = torch.load(
+            os.path.join(checkpoint_dir, "checkpoint"))
+        net.load_state_dict(model_state)
+        optimizer.load_state_dict(opt_state)
+    
+
+
+
+    for epoch in range(num_epochs):
+        running_loss = 0.0
+
+        # Train
+        for i,data in enumerate(trainloader, 0):
+            inputs, labels = data
+            inputs, labels = inputs.to(device), labels.to(device)
+
+            optimizer.zero_grad()
+
+            outputs = net(inputs)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+
+            # print statistics
+            running_loss += loss.item()
+            k = 200
+            if i % k == k-1:
+                print("[%d, %5d] loss: %.3f" % (epoch + 1, i + 1,
+                                                running_loss / k))
+                running_loss = 0.0
+
+        # Validate
+        val_loss = 0.0
+        val_steps = 0
+        total, correct = 0, 0
+        for i, data in enumerate(valloader, 0):
+            with torch.no_grad():
+                inputs, labels = data
+                inputs, labels = inputs.to(device), labels.to(device)
+
+                outputs = net(inputs)
+                _, predicted = torch.max(outputs.data, 1)
+                total += labels.size(0)
+                correct += (predicted == labels).sum().item()
+
+                loss = criterion(outputs, labels)
+                val_loss += loss.cpu().numpy()
+                val_steps += 1
+
+        with tune.checkpoint_dir(epoch) as checkpoint_dir:
+            path = os.path.join(checkpoint_dir, "checkpoint")
+            torch.save((net.state_dict(), optimizer.state_dict()), path)
+
+        tune.report(loss=(val_loss/val_steps), accuracy=correct/total)
+    print("finished training")
+
+
+
+
+
+# Test set accuracy
+
+def test_accuracy(net, device="cpu"):
+    _, testset = load_data()
+
+    testloader = torch.utils.data.DataLoader(
+        testset, batch_size=4, shuffle=False, num_workers=2)
+
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        for data in testloader:
+            images, labels = data
+            images, labels = images.to(device), labels.to(device)
+            outputs = net(images)
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+
+    return correct / total
+
+
+
+
+def main(num_samples=10, max_num_epochs=10):
+    data_dir = os.path.abspath("./data")
+    load_data(data_dir)
+    config = {
+        "l1": tune.sample_from(lambda _: 2 ** np.random.randint(2, 9)),
+        "l2": tune.sample_from(lambda _: 2 ** np.random.randint(2, 9)),
+        "lr": tune.loguniform(1e-4, 1e-2),
+        "batch_size": tune.choice([8, 16, 32])
+    }
+    scheduler = ASHAScheduler(
+        metric="loss",
+        mode="min",
+        max_t=max_num_epochs,
+        grace_period=1,
+        reduction_factor=2
+    )
+    reporter = CLIReporter(
+        parameter_columns=["l1", "l2", "lr", "batch_size"],
+        metric_columns=["loss", "accuracy", "training_iteration"]
+    )
+    result = tune.run(
+        partial(train_net, data_dir=data_dir),
+        resources_per_trial={"cpu": 2, "gpu": 1},
+        config=config,
+        num_samples=num_samples,
+        scheduler=scheduler,
+        progress_reporter=reporter
+    )
+
+    best_trial = result.get_best_trial("loss", "min", "last")
+    print("Best trial config: {}".format(best_trial.config))
+    print("Best trial final validation loss: {}".format(
+        best_trial.last_result["loss"]))
+    print("Best trial final validation accuracy: {}".format(
+        best_trial.last_result["accuracy"]))
+
+    best_trained_model = Net(best_trial.config["l1"], best_trial.config["l2"])
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    best_trained_model.to(device)
+
+    best_checkpoint_dir = best_trial.checkpoint.value
+    model_state, optimizer_state = torch.load(os.path.join(
+        best_checkpoint_dir, "checkpoint"))
+    best_trained_model.load_state_dict(model_state)
+
+    test_acc = test_accuracy(best_trained_model, device)
+    print("Best trial test set accuracy: {}".format(test_acc))
+
+
+if __name__ == "__main__":
+    main(num_samples=6, max_num_epochs=1)
+
